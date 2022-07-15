9c87a27c66c9c1750943dceecd3bf4fff42ddf89
Linda Hanson <107373537+Linda-Hanson@users.noreply.github.com>
2022-07-15 14:16:24 +0000

    attempt 2

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,14 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+
+// if (sumOfNumbers && typeof sumOfNumbers === 'function'){
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum= arrayOfNumbers.reduce((a,b)=> a+b,0);
+    return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +32,10 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let counteven = arrayOfNumbers.filter(number => { return number % 2 === 0; });
+    return counteven.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
